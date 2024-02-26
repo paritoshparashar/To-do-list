19a32fc67f9ece3cbf16b345dab94a056de84745
Paritosh Parashar <parasharparitosh2003@gmail.com>
2024-02-27 00:44:08 +0100

    Create a project class to create new Project

---
+++ javascript/projectCreation.js
@@ -0,0 +1,23 @@
+class newProject {
+
+    constructor (name){
+        this.name = name;
+    }
+
+    addNewTask (taskName){
+
+    }
+
+    editTask (taskName) {
+
+    }
+
+    removeTask (taskName){
+
+
+    }
+
+    markTaskCompleted (taskName) {
+
+    }
+}
