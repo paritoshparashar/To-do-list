efcbaf4ed9c8df92346343fdd2e091f0cdc88c20
paritoshparashar <parasharparitosh2003@gmail.com>
2024-03-19 12:02:08 +0100

    Restructure the code to use Live server and divide the config files into two seperate production and development

---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge (common , {
+
+    mode : 'production',
+
+    output : {
+        filename : "main.prod.js",
+        path : path.resolve(__dirname , "dist")
+    }
+
+});
