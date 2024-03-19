7ad3899bf9420ebe9d3f826b67eabe16fbf9928a
paritoshparashar <parasharparitosh2003@gmail.com>
2024-03-19 12:08:30 +0100

    Bundle html files together

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,12 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
     entry : "./src/index.js",
+
+    plugins: [new HtmlWebpackPlugin({
+        template : "./src/template.html",
+    })],
     
 };
